635006242ed2db14bc3781af67035ddf3c9c9401
JustickDM <maxjust@inbox.ru>
2020-10-26 14:02:59 +0300

    .

--- NodejsWebApp/server.js
+++ NodejsWebApp/server.js
@@ -2,11 +2,27 @@
 
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
 var port = process.env.PORT || 1337;
 
-app.get("/", function (request, response) {
+app.use(function (request, response, next)
+{
+    var now = new Date();
+    var hour = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
 
+    console.log(data);
+
+    fs.appendFile("server.log", data + "\n", function () { });
+
+    next();
+});
+
+app.get("/", function (request, response)
+{
     response.end("Hello from Express!");
 });
 
