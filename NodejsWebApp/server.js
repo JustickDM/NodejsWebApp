383510b58632d6881f60f4f6b3067bcd3d1df044
JustickDM <maxjust@inbox.ru>
2020-10-26 16:40:00 +0300

    .

--- NodejsWebApp/server.js
+++ NodejsWebApp/server.js
@@ -7,26 +7,14 @@ var dbUri = "mongodb://localhost:27017/";
 
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
-const fs = require("fs");
+const logger = require("./Modules/logger");
 const app = express();
 const mongoClient = new MongoClient(dbUri, { useNewUrlParser: true });
-
-function writeLog(message, logPath) {
-    var now = new Date();
-    var hour = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
-    var data = `${hour}:${minutes}:${seconds} message: ${message}}`;
-
-    console.log(data);
-
-    fs.appendFile(logPath, data + "\n", function () { });
-};
 
 app.use(function (request, response, next){  
     var data = `${request.method} ${request.url} ${request.get("user-agent")}`;
 
-    writeLog(data, serverLogsPath);
+    logger.writeLog(data, serverLogsPath);
 
     next();
 });
@@ -39,7 +27,7 @@ app.get("/Users", function (request, response){
     mongoClient.connect(function (error, client) {
 
         if (error) {
-            writeLog(error, dbLogsPath);
+            logger.writeLog(error, dbLogsPath);
 
             response.sendStatus(418);
         }
@@ -50,7 +38,7 @@ app.get("/Users", function (request, response){
             collection.find().toArray(function (error, results) {
 
                 if (error) {
-                    writeLog(error, dbLogsPath);
+                    logger.writeLog(error, dbLogsPath);
                 }
 
                 response.send(results);
